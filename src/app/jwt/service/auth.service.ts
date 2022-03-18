832b1845bbc901c393436514527a883eaeb3a612
gastonm1998 <gastonmonasterio1998@gmail.com>
2022-03-17 21:42:13 -0300

    agregado los modelos jwt y los service

--- src/app/jwt/service/auth.service.ts
+++ src/app/jwt/service/auth.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginUsuario } from '../modelos/login-usuario';
+import { NuevoUsuario } from '../modelos/nuevo-usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  authURL = "http://localhost:8080/auth/"
+
+  constructor(private httpClient: HttpClient ) { }
+
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+    return this.httpClient.post<any>(this.authURL + "nuevo" , nuevoUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<any>{
+    return this.httpClient.post<any>(this.authURL + "nuevo" , loginUsuario);
+  }
 }
